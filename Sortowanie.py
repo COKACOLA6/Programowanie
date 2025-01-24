d09b893db4a2a462216b47687cdc4c11be8712da
Leon <leon777cio@gmail.com>
2025-01-24 14:44:12 +0100

    Funkcje

---
+++ Sortowanie.py
@@ -0,0 +1,36 @@
+"""utworzenie tablicy z losowymi elementami - liczbami od 0 do 99 typu int
+
+funkcja która posortuje nam tablicę i zwróci ją jako wynik
+ 
+do zwracanaia użyjemy return
+ 
+funkcja do porónania wników i sprawdzenia czy wynik jest prawidłowy - porównanie dwóch list ze sobą
+ 
+funkcja - wypisanie dwóch tablic jedna pod drugą
+ 
+funkcja main - w któej będą wywoływane wszystkie poprzednie funkcje
+"""
+
+import math
+import random
+random.seed(1)
+
+
+def sortowanie():
+    pass
+
+
+def losowanie_tablicy():
+    pass
+
+
+def porownanie():
+    pass
+
+
+def wypisanie_tablic():
+    pass
+
+
+def main():
+    pass
