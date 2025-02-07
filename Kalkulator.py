88be449d5e49cc6f768cb89006a3edc6a1a9a3c7
Leon <leon777cio@gmail.com>
2025-02-07 15:00:50 +0100

    Opcje

---
+++ Kalkulator.py
@@ -0,0 +1,16 @@
+import os
+from euklides import NWD
+#from Sortowanie import *
+import Sortowanie
+#Clearing the Screenos.
+os.system('cls')
+liczba = int(input("Podaj pierwsza liczbe do sumy"))
+liczba2 = int(input("Podaj druga liczbe do sumy"))
+opcje = int(input("Wybierz Opcje 1- NWD, 2-Suma, 3-sortowanie"))
+while opcje>=1 and opcje<=3:
+    if opcje == 1:
+        print("NWD:",NWD(liczba,liczba2))
+    if opcje == 2:
+        print("Suma:",liczba + liczba2)
+    if opcje == 3:
+        print("Sortowanie:",Sortowanie.babelkowe(tab = Sortowanie.losowanie_tablicy(5,0,99)))
